fef26c06d6565346f27629da4c48332b286e56f1
oldmartijntje <oldmartijntje@gmail.com>
2023-06-13 14:34:11 +0200

    home page dynamic

--- spotify-hub/src/app/app.module.ts
+++ spotify-hub/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- spotify-hub/src/app/pages/home-page/home-page.component.ts
+++ spotify-hub/src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+    selector: 'app-home-page',
+    templateUrl: './home-page.component.html',
+    styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
-
+    Articles = [
+        { "title": "New feature!", "text": "You can now dye your hair purple.", "class": "highlight", "image": { "enabled": true, "path": "assets/icons/OceanLogo.png", "altText": "An image to show the feature.", "bottomText": "This is an example Image" } }
+    ];
+    length = 1;
+    addArticle() {
+        var title = "message number " + this.length;
+        var tex = "meow " + this.length + " times for me please OwO";
+        this.length++;
+        if (this.length % 2 == 0) {
+            var clas = "highlight";
+        } else {
+            var clas = "";
+        }
+        if (this.length % 3 == 0) {
+            var img = true;
+        } else {
+            var img = false;
+        }
+        this.Articles.push({ "title": title, "text": tex, "class": clas, "image": { "enabled": img, "path": "assets/icons/OceanLogo.png", "altText": "An image to show the feature.", "bottomText": "This is an example Image" } });
+    }
 }
