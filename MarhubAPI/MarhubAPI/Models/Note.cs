b5f0a519c4a25a221e043208f4bbd1be42c17355
oldmartijntje <oldmartijntje@gmail.com>
2023-08-11 11:05:53 +0200

    API start

---
+++ MarhubAPI/MarhubAPI/Models/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace MarhubAPI.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public string NoteText { get; set; } = string.Empty;
+        public string ElementType { get; set; } = string.Empty;
+        public string ElementId { get; set; } = string.Empty;
+    }
+}
